eb53b1de33d081502fff6a468d020cce92b4c78f
xuyanMax <418780414@qq.com>
2016-04-21 13:25:34 +0800

    add 2 files

---
+++ index.php
@@ -0,0 +1,44 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title> To do </title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/main.css">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    </head>
+    
+        <div class="list">
+            <h1 class="header">To do.</h1>
+<!--            <?php if(!empty($items)): ?>-->
+            <ul class="items">
+<!--               <?php foreach($items as $item): ?>-->
+                <li>
+                    <span class="item">Pick up shopping</span>
+<!--                        <?php if(!$item['done']): ?>-->
+                        <a href="#" class="done-button">Mark as done</a>
+<!--                        <?php endif; ?>-->
+                </li>
+<!--               <?php endforeach; ?>-->
+            </ul>
+<!--            <?php else: ?>-->
+                    <p>You haven't added any items yet.</p>
+<!--            <?php endif ?>-->
+            
+            
+            <form class="item-add"action="add.php" method="post">
+                <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
+                <input type="submit" value="Add" class="submit"> 
+            
+            
+            </form>
+      </div>
+    <body>
+    </body>
+    
+
+</html>
---
+++ index2.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title> To do </title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/main.css">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    </head>
+    
+        <div class="list">
+            <h1 class="header">To do.</h1>
+
+            <ul class="items">
+
+                <li>
+                    <span class="item">Pick up shopping</span>
+
+                        <a href="#" class="done-button">Mark as done</a>
+
+                </li>
+
+            </ul>
+
+                    <p>You haven't added any items yet.</p>
+
+            
+            
+            <form class="item-add"action="add.php" method="post">
+                <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
+                <input type="submit" value="Add" class="submit"> 
+            
+            
+            </form>
+      </div>
+    <body>
+    </body>
+    
+
+</html>
